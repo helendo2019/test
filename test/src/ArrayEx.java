81193b82f46ab327e8b77628cbf476eb4d0e804a
Helen <helen2019@gmail.com>
2020-12-12 19:37:12 -0800

    1st

---
+++ test/src/ArrayEx.java
@@ -0,0 +1,15 @@
+
+public class ArrayEx {
+
+	public static void main(String[] args) {
+		int arr[]={10,20,30,40,50};
+		//how to get this elements from array
+		for(int pos=0;pos<arr.length;pos++)
+		{
+			System.out.println(arr[pos]);
+		}
+		
+
+	}
+
+}
